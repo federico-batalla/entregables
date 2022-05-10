7e6ece5a1592d668f6a69ed80f17c9103eea2f89
Federico Batalla <104173891+federico-batalla@users.noreply.github.com>
2022-05-09 23:43:15 -0300

    ejercicio ciclos

---
+++ src/ciclosRepaso.ts
@@ -0,0 +1,20 @@
+let valor: number = Number(prompt("introduzca valor"));
+let contador: number = 0;
+let ceros: number = 0;
+while (valor !== 0) {
+  contador = contador + 1;
+  console.log(contador);
+  if (valor > 0) {
+    ceros = ceros + 1;
+    console.log(ceros);
+  }
+  valor = Number(prompt("introduzca valor"));
+}
+let porcentaje: number = (ceros * 100) / contador;
+console.log(
+  "cantidad denumeros > 0 ingresados: ",
+  ceros,
+  "es el ",
+  porcentaje,
+  " % del total"
+);
