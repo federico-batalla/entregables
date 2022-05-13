ee158eccba8174d44909efc0dde5e90697d17b28
Federico Batalla <104173891+federico-batalla@users.noreply.github.com>
2022-05-12 23:27:56 -0300

    se agrego la funcion

--- src/calculadora.ts
+++ src/calculadora.ts
@@ -1,3 +1,12 @@
+//funcion que dibuja las lineas
+function dibujarLineas() {
+  let linea: string = "-";
+  for (let index = 0; index < 40; index++) {
+    linea = linea + "-";
+  }
+  console.log(linea);
+}
+// declaracion de variables
 let numero1: number = Number(prompt("ingrese 1er Numero"));
 let numero2: number = Number(prompt("ingrese 2do Numero"));
 let resultado: number = 0;
@@ -8,19 +17,27 @@ let opcion: number = Number(
 switch (opcion) {
   case 1:
     resultado = numero1 + numero2;
+    dibujarLineas();
     console.log("el resultado de la suma es :", resultado);
+    dibujarLineas();
     break;
   case 2:
     resultado = numero1 - numero2;
+    dibujarLineas();
     console.log("el resultado de la resta es :", resultado);
+    dibujarLineas();
     break;
   case 3:
     resultado = numero1 * numero2;
+    dibujarLineas();
     console.log("el resultado de la multiplicacion es :", resultado);
+    dibujarLineas();
     break;
   case 4:
     resultado = numero1 / numero2;
+    dibujarLineas();
     console.log("el resultado de la division es :", resultado);
+    dibujarLineas();
     break;
   default:
     console.log("ingreso un  numero invalido");
