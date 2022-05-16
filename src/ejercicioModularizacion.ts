e5ccc954581392f8addf10b31430d8b7b05fb7c4
Federico Batalla <104173891+federico-batalla@users.noreply.github.com>
2022-05-15 22:36:23 -0300

    terminado

---
+++ src/ejercicioModularizacion.ts
@@ -0,0 +1,26 @@
+function esMultiplo(dividendo: number, divisor: number): boolean {
+  let resto: number = dividendo % divisor;
+  if (resto === 0) {
+    return true;
+  }
+  return false;
+}
+
+//--------------------------------------------------------
+function cantidadDeDivisores(dividendo: number): number {
+  let cantidadDivisores: number = 0;
+  for (let indice = 1; indice <= dividendo; indice++) {
+    if (esMultiplo(dividendo, indice)) {
+      cantidadDivisores++;
+    }
+  }
+  return cantidadDivisores;
+}
+//----------Programa Principal-------------------------
+let numeroIngresado: number = Number(prompt("ingrese un numero"));
+console.log(
+  "la cantidad de divisores de ",
+  numeroIngresado,
+  "es",
+  cantidadDeDivisores(numeroIngresado)
+);
